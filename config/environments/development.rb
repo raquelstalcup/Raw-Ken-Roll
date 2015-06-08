8969226f246af3674e749f2a3719214dcd9d7ddb
Raquel Stalcup <raquel.stalcup@careerbuilder.com>
2015-06-08 11:19:36 -0400

    gemfile remove sqlite3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
   # Code is not reloaded between requests.
   config.cache_classes = true
